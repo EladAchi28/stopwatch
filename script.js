4441a48dd7c590d1775c9cb6b1b22e912ba0f618
EladAchi28 <131887561+EladAchi28@users.noreply.github.com>
2023-04-26 18:49:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
-const timer1 = document.getElementById('timer1');
-const startStop1 = document.getElementById('startStop1');
-const reset1 = document.getElementById('reset1');
+document.addEventListener('DOMContentLoaded', () => {
+  const timer1 = document.getElementById('timer1');
+  const startStop1 = document.getElementById('startStop1');
+  const reset1 = document.getElementById('reset1');
 
-const timer2 = document.getElementById('timer2');
-const startStop2 = document.getElementById('startStop2');
-const reset2 = document.getElementById('reset2');
+  const timer2 = document.getElementById('timer2');
+  const startStop2 = document.getElementById('startStop2');
+  const reset2 = document.getElementById('reset2');
 
-let isRunning1 = false;
-let startTime1;
-let elapsedTime1 = 0;
-let timerInterval1;
+  let startTime1;
+  let startTime2;
+  let elapsedTime1 = 0;
+  let elapsedTime2 = 0;
+  let timerInterval1;
+  let timerInterval2;
+  let isRunning1 = false;
+  let isRunning2 = false;
 
-let isRunning2 = false;
-let startTime2;
-let elapsedTime2 = 0;
-let timerInterval2;
+  function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
 
-function timeToString(time) {
-  let diffInHrs = time / 3600000;
-  let hh = Math.floor(diffInHrs);
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
 
-  let diffInMin = (diffInHrs - hh) * 60;
-  let mm = Math.floor(diffInMin);
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
 
-  let diffInSec = (diffInMin - mm) * 60;
-  let ss = Math.floor(diffInSec);
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
 
-  let diffInMs = (diffInSec - ss) * 100;
-  let ms = Math.floor(diffInMs);
+    let formattedMM = mm.toString().padStart(2, '0');
+    let formattedSS = ss.toString().padStart(2, '0');
+    let formattedMS = ms.toString().padStart(2, '0');
 
-  let formattedHH = hh.toString().padStart(2, '0');
-  let formattedMM = mm.toString().padStart(2, '0');
-  let formattedSS = ss.toString().padStart(2, '0');
-  let formattedMS = ms.toString().padStart(2, '0');
+    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+  }
 
-  return `${formattedMM}:${formattedSS}:${formattedMS}`;
-}
-
-function startStopTimer1() {
-  if (!isRunning1) {
-    startTime1 = Date.now() - elapsedTime1;
-    timerInterval1 = setInterval(() => {
-      elapsedTime1 = Date.now() - startTime1;
-      timer1.textContent = timeToString(elapsedTime1);
-    }, 10);
-    startStop1.textContent = 'Stop';
-    isRunning1 = true;
-  } else {
+  function startStopTimer1() {
+    if (!isRunning1) {
+      startTime1 = Date.now() - elapsedTime1;
+      timerInterval1 = setInterval(function printTime() {
+        elapsedTime1 = Date.now() - startTime1;
+        timer1.textContent = timeToString(elapsedTime1);
+      }, 10);
+      startStop1.textContent = 'Stop';
+      isRunning1 = true;
+   } else {
     clearInterval(timerInterval1);
     startStop1.textContent = 'Start';
     isRunning1 = false;
     updateImprovementPercentage();
   }
-}
 
-function resetTimer1() {
+  function resetTimer1() {
   clearInterval(timerInterval1);
   startStop1.textContent = 'Start';
   isRunning1 = false;
@@ -63,16 +61,16 @@ function resetTimer1() {
   updateImprovementPercentage();
 }
 
-function startStopTimer2() {
-  if (!isRunning2) {
-    startTime2 = Date.now() - elapsedTime2;
-    timerInterval2 = setInterval(() => {
-      elapsedTime2 = Date.now() - startTime2;
-      timer2.textContent = timeToString(elapsedTime2);
-    }, 10);
-    startStop2.textContent = 'Stop';
-    isRunning2 = true;
-  } else {
+  function startStopTimer2() {
+    if (!isRunning2) {
+      startTime2 = Date.now() - elapsedTime2;
+      timerInterval2 = setInterval(function printTime() {
+        elapsedTime2 = Date.now() - startTime2;
+        timer2.textContent = timeToString(elapsedTime2);
+      }, 10);
+      startStop2.textContent = 'Stop';
+      isRunning2 = true;
+   } else {
     clearInterval(timerInterval2);
     startStop2.textContent = 'Start';
     isRunning2 = false;
@@ -80,7 +78,7 @@ function startStopTimer2() {
   }
 }
 
-function resetTimer2() {
+ function resetTimer2() {
   clearInterval(timerInterval2);
   startStop2.textContent = 'Start';
   isRunning2 = false;
@@ -88,6 +86,15 @@ function resetTimer2() {
   timer2.textContent = '00:00:00';
   updateImprovementPercentage();
 }
+
+  startStop1.addEventListener('click', startStopTimer1);
+  reset1.addEventListener('click', resetTimer1);
+
+  startStop2.addEventListener('click', startStopTimer2);
+  reset2.addEventListener('click', resetTimer2);
+});
+
+
 
 function updateImprovementPercentage() {
   if (elapsedTime1 > 0 && elapsedTime2 > 0) {
@@ -98,6 +105,3 @@ function updateImprovementPercentage() {
     document.getElementById('improvementPercentage').textContent = '0%';
   }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  startStop
