863335fb2ef70ea0e920e591c144cce5f85d85d5
EladAchi28 <131887561+EladAchi28@users.noreply.github.com>
2023-04-26 19:52:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,107 @@
-const timer1 = document.getElementById('timer1');
-const startStop1 = document.getElementById('startStop1');
-const reset1 = document.getElementById('reset1');
+$(document).ready(() => {
+  let running1 = false,
+    running2 = false,
+    time1 = 0,
+    time2 = 0,
+    interval1,
+    interval2,
+    startTime1,
+    startTime2;
 
-const timer2 = document.getElementById('timer2');
-const startStop2 = document.getElementById('startStop2');
-const reset2 = document.getElementById('reset2');
+  const timer1 = $('#timer1'),
+    timer2 = $('#timer2'),
+    startStop1 = $('#startStop1'),
+    startStop2 = $('#startStop2'),
+    reset1 = $('#reset1'),
+    reset2 = $('#reset2'),
+    improvement = $('#improvement');
 
-const improvement = document.getElementById('improvement');
+  function updateTime1() {
+    const currentTime = Date.now();
+    time1 += currentTime - startTime1;
+    startTime1 = currentTime;
+    timer1.text(formatTime(time1));
+  }
 
-let running1 = false;
-let running2 = false;
+  function startStopTimer1() {
+    if (running1) {
+      clearInterval(interval1);
+      startStop1.text('Start');
+    } else {
+      startTime1 = Date.now();
+      interval1 = setInterval(updateTime1, 10);
+      startStop1.text('Stop');
+    }
+    running1 = !running1;
+  }
 
-let time1 = 0;
-let time2 = 0;
-
-let interval1;
-let interval2;
-
-function updateTime1() {
-  const minutes = Math.floor(time1 / 6000);
-  const seconds = Math.floor((time1 % 6000) / 100);
-  const centiseconds = time1 % 100;
-
-  timer1.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${centiseconds.toString().padStart(2, '0')}`;
-  time1++;
-}
-
-function updateTime2() {
-  const minutes = Math.floor(time2 / 6000);
-  const seconds = Math.floor((time2 % 6000) / 100);
-  const centiseconds = time2 % 100;
-
-  timer2.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${centiseconds.toString().padStart(2, '0')}`;
-  time2++;
-}
-
-function startStopTimer1() {
-  if (running1) {
+  function resetTimer1() {
     clearInterval(interval1);
-    startStop1.textContent = 'Start';
-  } else {
-    interval1 = setInterval(updateTime1, 10);
-    startStop1.textContent = 'Stop';
+    running1 = false;
+    time1 = 0;
+    timer1.text(formatTime(time1));
+    startStop1.text('Start');
   }
-  running1 = !running1;
-}
 
-function startStopTimer2() {
-  if (running2) {
+  function updateTime2() {
+    const currentTime = Date.now();
+    time2 += currentTime - startTime2;
+    startTime2 = currentTime;
+    timer2.text(formatTime(time2));
+  }
+
+  function startStopTimer2() {
+    if (running2) {
+      clearInterval(interval2);
+      startStop2.text('Start');
+      if (time2 > 0) {
+        calculateImprovement();
+      }
+    } else {
+      startTime2 = Date.now();
+      interval2 = setInterval(updateTime2, 10);
+      startStop2.text('Stop');
+    }
+    running2 = !running2;
+  }
+
+  function resetTimer2() {
     clearInterval(interval2);
-    startStop2.textContent = 'Start';
-  } else {
-    interval2 = setInterval(updateTime2, 10);
-    startStop2.textContent = 'Stop';
+    running2 = false;
+    time2 = 0;
+    timer2.text(formatTime(time2));
+    startStop2.text('Start');
+    improvement.empty();
   }
-  running2 = !running2;
-}
 
-function resetTimer1() {
-  clearInterval(interval1);
-  time1 = 0;
-  timer1.textContent = '00:00:00';
-  startStop1.textContent = 'Start';
-  running1 = false;
-}
+  function formatTime(time) {
+    const date = new Date(time);
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+    const milliseconds = Math.floor(date.getUTCMilliseconds() / 10)
+      .toString()
+      .padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  }
 
-function resetTimer2() {
-  clearInterval(interval2);
-  time2 = 0;
-  timer2.textContent = '00:00:00';
-  startStop2.textContent = 'Start';
-  running2 = false;
-}
+  function calculateImprovement() {
+    const time1InSeconds = time1 / 1000;
+    const time2InSeconds = time2 / 1000;
+    const improvementInSeconds = time1InSeconds - time2InSeconds;
+    const improvementPercentage = Math.round(
+      (improvementInSeconds / time1InSeconds) * 100
+    );
+    improvement.html(
+      `Improvement: <span>${improvementPercentage}% (${formatTime(
+        improvementInSeconds * 1000
+      )})</span>`
+    );
+  }
 
-function calculateImprovement() {
-  const time1Arr = timer1.textContent.split(':');
-  const time1Seconds = (+time1Arr[0]) * 60 * 60 + (+time1Arr[1]) * 60 + (+time1Arr[2]);
-  
-  const time2Arr = timer2.textContent.split(':');
-  const time2Seconds = (+time2Arr[0]) * 60 * 60 + (+time2Arr[1]) * 60 + (+time2Arr[2]);
-  
-  const improvementPercent = Math.round(((time1Seconds - time2Seconds) / time1Seconds) * 100);
-  improvement.textContent = `Improvement: ${improvementPercent}%`;
-}
+  startStop1.on('click', startStopTimer1);
+  reset1.on('click', resetTimer1);
 
-startStop1.addEventListener('click', startStopTimer1);
-reset1.addEventListener('click', resetTimer1);
-
-startStop2.addEventListener('click', startStopTimer2);
-reset2.addEventListener('click', resetTimer2);
-
-setInterval(calculateImprovement, 100);
+  startStop2.on('click', startStopTimer2);
+  reset2.on('click', resetTimer2);
+});
