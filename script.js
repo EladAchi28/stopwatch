7573de531b5284cc179ff37d7f771504c8173aa2
EladAchi28 <131887561+EladAchi28@users.noreply.github.com>
2023-04-27 14:04:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,124 +1,163 @@
 $(document).ready(() => {
-  let running1 = false,
-    running2 = false,
-    time1 = 0,
-    time2 = 0,
-    interval1,
-    interval2,
-    startTime1,
-    startTime2;
+const currentMethod = $('.currentMethod');
+const currentMood = $('.currentMood');
 
-  const timer1 = $('#timer1'),
-    timer2 = $('#timer2'),
-    startStop1 = $('#startStop1'),
-    startStop2 = $('#startStop2'),
-    reset1 = $('#reset1'),
-    reset2 = $('#reset2'),
-    improvement = $('#improvement'),
-    currentMethod = $('#currentMethod'),
-    currentMood = $('#currentMood'),
-    solutionBtn = $('#solutionBtn');
+const stopwatch1 = $('.stopwatch').eq(0);
+const stopwatch2 = $('.stopwatch').eq(1);
 
-  function updateTime1() {
-    const currentTime = Date.now();
-    time1 += currentTime - startTime1;
-    startTime1 = currentTime;
-    timer1.text(formatTime(time1));
-  }
+const startStop1 = stopwatch1.find('#startStop1');
+const startStop2 = stopwatch2.find('#startStop2');
 
-  function startStopTimer1() {
-    if (running1) {
-      clearInterval(interval1);
-      startStop1.text('Start');
-    } else {
-      startTime1 = Date.now();
-      interval1 = setInterval(updateTime1, 10);
-      startStop1.text('Stop');
-    }
-    running1 = !running1;
-  }
+const reset1 = stopwatch1.find('#reset1');
+const reset2 = stopwatch2.find('#reset2');
 
-  function resetTimer1() {
-    clearInterval(interval1);
-    running1 = false;
-    time1 = 0;
-    timer1.text(formatTime(time1));
+const time1Display = stopwatch1.find('.stopwatch__time');
+const time2Display = stopwatch2.find('.stopwatch__time');
+
+const improvement = $('.improvement__result');
+
+let time1 = 0;
+let time2 = 0;
+let timer1;
+let timer2;
+
+function formatTime(timeInMilliseconds) {
+  const date = new Date(timeInMilliseconds);
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const milliseconds = Math.floor(date.getMilliseconds() / 10)
+    .toString()
+    .padStart(2, '0');
+  return `${minutes}:${seconds}:${milliseconds}`;
+}
+
+function startTimer1() {
+  const startTime = Date.now() - time1;
+  timer1 = setInterval(() => {
+    time1 = Date.now() - startTime;
+    time1Display.html(formatTime(time1));
+    calculateImprovement();
+  }, 10);
+}
+
+function startTimer2() {
+  const startTime = Date.now() - time2;
+  timer2 = setInterval(() => {
+    time2 = Date.now() - startTime;
+    time2Display.html(formatTime(time2));
+    calculateImprovement();
+  }, 10);
+}
+
+function stopTimer1() {
+  clearInterval(timer1);
+}
+
+function stopTimer2() {
+  clearInterval(timer2);
+}
+
+function resetTimer1() {
+  stopTimer1();
+  time1 = 0;
+  time1Display.html(formatTime(time1));
+  improvement.html('Improvement:');
+}
+
+function resetTimer2() {
+  stopTimer2();
+  time2 = 0;
+  time2Display.html(formatTime(time2));
+  improvement.html('Improvement:');
+}
+
+function calculateImprovement() {
+  const time1InSeconds = time1 / 1000;
+  const time2InSeconds = time2 / 1000;
+  const improvementInSeconds = time1InSeconds - time2InSeconds;
+  const improvementPercentage = Math.round(
+    (improvementInSeconds / time1InSeconds) * 100
+  );
+  const improvementText = `Improvement: <span style="color: #f42b5b; font-family: Rubik">${improvementPercentage}% (${formatTime(
+    improvementInSeconds * 1000
+  )})</span>`;
+  improvement.html(improvementText);
+}
+
+startStop1.on('click', () => {
+  if (startStop1.text() === 'Start') {
+    startStop1.text('Stop');
+    startTimer1();
+  } else {
     startStop1.text('Start');
+    stopTimer1();
   }
-
-  function updateTime2() {
-    const currentTime = Date.now();
-    time2 += currentTime - startTime2;
-    startTime2 = currentTime;
-    timer2.text(formatTime(time2));
-  }
-
-  function startStopTimer2() {
-    if (running2) {
-      clearInterval(interval2);
-      startStop2.text('Start');
-      if (time2 > 0) {
-        calculateImprovement();
-      }
-    } else {
-      startTime2 = Date.now();
-      interval2 = setInterval(updateTime2, 10);
-      startStop2.text('Stop');
-    }
-    running2 = !running2;
-  }
-
-  function resetTimer2() {
-    clearInterval(interval2);
-    running2 = false;
-    time2 = 0;
-    timer2.text(formatTime(time2));
-    startStop2.text('Start');
-    improvement.empty();
-  }
-
-  function formatTime(time) {
-    const date = new Date(time);
-    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-    const seconds = date.getUTCSeconds().toString().padStart(2, '0');
-    const milliseconds = Math.floor(date.getUTCMilliseconds() / 10)
-      .toString()
-      .padStart(2, '0');
-    return `${minutes}:${seconds}:${milliseconds}`;
-  }
-
-  function calculateImprovement() {
-    const time1InSeconds = time1 / 1000;
-    const time2InSeconds = time2 / 1000;
-    const improvementInSeconds = time1InSeconds - time2InSeconds;
-    const improvementPercentage = Math.round(
-      (improvementInSeconds / time1InSeconds) * 100
-    );
-    const improvementText = `Improvement: <span style="color: #f42b5b; font-family: Rubik">${improvementPercentage}% (${formatTime(
-      improvementInSeconds * 1000
-    )})</span>`;
-    improvement.html(improvementText);
-  }
-
-  startStop1.on('click', startStopTimer1);
-  reset1.on('click', resetTimer1);
-
-  startStop2.on('click', startStopTimer2);
-  reset2.on('click', resetTimer2);
-
-   currentMethod.css('color', '#fff');
-  currentMood.css('color', '#fff');
-
-  solutionBtn.css({
-    'margin-top': '50px',
-    'margin-bottom': '50px',
-    'display': 'block',
-    'margin-left': 'auto',
-    'margin-right': 'auto'
-  });
-  
-  solutionBtn.on('click', () => {
-    window.location.href = 'https://docs.google.com/presentation/d/1A7fNQXmaXX3sYC_lhAn2J0t_BhSIFKW3CCDDDgucR08/edit#slide=id.g23a1fb12a21_3_50';
-  });
-
 });
+
+reset1.on('click', resetTimer1);
+
+startStop2.on('click', () => {
+  if (startStop2.text() === 'Start') {
+    startStop2.text('Stop');
+    startTimer2();
+  } else {
+    startStop2.text('Start');
+    stopTimer2();
+  }
+});
+
+reset2.on('click', resetTimer2);
+
+currentMethod.css('color', '#fff');
+currentMood.css('color', '#fff');
+
+solutionBtn.on('click', () => {
+  window.location.href = 'https://docs.google.com/presentation/d/1A7fNQXmaXX3sYC_lhAn2J0t_BhSIFKW3CCDDDgucR08/edit#slide=id.g23a1fb12a21_3_50';
+});
+
+function startTimer1() {
+  startTime1 = Date.now() - time1;
+  interval1 = setInterval(() => {
+    time1 = Date.now() - startTime1;
+    timer1.html(formatTime(time1));
+  }, 10);
+}
+
+function stopTimer1() {
+  clearInterval(interval1);
+}
+
+function resetTimer1() {
+  time1 = 0;
+  timer1.html(formatTime(time1));
+  startStop1.text('Start');
+  stopTimer1();
+}
+
+function startTimer2() {
+  startTime2 = Date.now() - time2;
+  interval2 = setInterval(() => {
+    time2 = Date.now() - startTime2;
+    timer2.html(formatTime(time2));
+  }, 10);
+}
+
+function stopTimer2() {
+  clearInterval(interval2);
+}
+
+function resetTimer2() {
+  time2 = 0;
+  timer2.html(formatTime(time2));
+  startStop2.text('Start');
+  stopTimer2();
+}
+
+function formatTime(time) {
+  const date = new Date(time);
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+  const milliseconds = Math.floor(date.getUTCMilliseconds() / 10).toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+}
